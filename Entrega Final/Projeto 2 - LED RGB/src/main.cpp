88ca23578bb45e72b96a4e9baf0c09481ed6b082
Thallys Oliveira <thallys.smo@gmail.com>
2024-06-29 15:32:24 -0300

    Entrega Final - Serial LED

--- Entrega Final/Projeto 2 - LED RGB/src/main.cpp
+++ Entrega Final/Projeto 2 - LED RGB/src/main.cpp
@@ -4,44 +4,52 @@
 #define G 5
 #define B 16
 
-String Serialdata = "";
-bool dataflag = 0;
 int DutyCycle_R = 0;
 int DutyCycle_G = 0;
 int DutyCycle_B = 0;
-int increment = 1; // Declaração da variável incremento
+int increment = 1; // Declaration of the increment variable
 
 void setup() {
-  Serial.begin(115200); // Configura a comunicação serial com uma taxa de transmissão de dados de 115200 bits por segundo.
+  Serial.begin(115200); // Configures serial communication with a data transmission rate of 115200 bits per second.
   
-    // Inicializar as saídas PWM para controle do LED
+  // Initialize the PWM outputs for LED control
   pinMode(R, OUTPUT);
   pinMode(G, OUTPUT);
   pinMode(B, OUTPUT);
 }
 
 void loop() {
-  // Atualizar o duty cycle para as cores do LED
+  // Update duty cycle for LED colors
   analogWrite(R, DutyCycle_R);
   analogWrite(G, DutyCycle_G);
   analogWrite(B, DutyCycle_B);
-
-  // Aumentar o duty cycle de cada canal
+  
+  // Increase the duty cycle of each channel
   DutyCycle_R += 2 * increment;
   if (DutyCycle_R > 255) {
     DutyCycle_R = 0;
   }
-
+  
   DutyCycle_G += increment;
   if (DutyCycle_G > 255) {
     DutyCycle_G = 0;
   }
-
+  
   DutyCycle_B += 3 * increment;
   if (DutyCycle_B > 255) {
     DutyCycle_B = 0;
-  } 
-
-  // Aguardar um pequeno intervalo antes de atualizar o ciclo de trabalho novamente
+  }
+  
+  // Print increment and duty cycle values to Serial Monitor
+  Serial.print("Increment: ");
+  Serial.print(increment);
+  Serial.print(", DutyCycle_R: ");
+  Serial.print(DutyCycle_R);
+  Serial.print(", DutyCycle_G: ");
+  Serial.print(DutyCycle_G);
+  Serial.print(", DutyCycle_B: ");
+  Serial.println(DutyCycle_B);
+  
+  // Wait a short interval before updating the duty cycle again
   delay(100);
 }
